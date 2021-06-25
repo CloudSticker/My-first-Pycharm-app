b2c75d97d11a43ad8583d9f52a1cf318f90d88e3
Daniil <dan9154634874@gmail.com>
2021-06-25 15:07:20 +0300

    Add animation cells and circles

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-from tqdm import tqdm
-import time
 from tkinter import *
-
-
-# for i in tqdm(range(100)):
-#     time.sleep(0.1)
-
-def clearCanvas():
-    canv.clipboard_clear()
-
-
 root = Tk()
+root.title("Михаил Агафонов")
 
-canv = Canvas(root, width=1000, height=500, bg='white')
+
+def clearCanvas(n, canvWidth, canvHeight, array):
+    newCanv = Canvas(root, width=canvWidth, height=canvHeight, bg='white')
+    yStart = canvHeight / 2 - (1 / 2) * canvWidth / n
+    for i in range(0, len(array)):
+        newCanv.create_line((canvWidth / n) * i, yStart, (canvWidth / n) * i, canvWidth / n + yStart, width=3)
+        if array[i] == 1:
+            newCanv.create_oval((canvWidth / n) * i + 5, yStart + 5, (canvWidth / n) * i + (canvWidth / n) - 5, canvWidth / n + yStart - 5, width=3)
+    newCanv.create_line(0, canvWidth / n + yStart, canvWidth, canvWidth / n + yStart, width=5)
+    newCanv.create_line(0, yStart, canvWidth, yStart, width=5)
+    return newCanv
+
+
+col = 10
+arr = []
+for i in range(0, col):
+    arr.append(i % 2)
+
+
+canv = clearCanvas(col, 1000, 300, arr)
 canv.pack()
-canv.create_line(10, 10, 190, 50)
-# clearCanvas()
-root.destroy()
-canv.create_line(100, 100, 1900, 500)
 root.mainloop()
-
-
-# def print_hi(name):
-#     print(f'Hi, {name}')
-#
-#
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
